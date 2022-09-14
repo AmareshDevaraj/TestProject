add7d0a61b9409c781b76b9af391986732e9ee78
Amaresh <rdamaresh@gmail.com>
2022-09-14 21:04:21 +0530

    StringFunctionEx1 class file commit

---
+++ TestProject/src/com/javatpoint/StringFunctionEx1.java
@@ -0,0 +1,11 @@
+package com.javatpoint;
+
+public class StringFunctionEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String aa = "Amaresh ";
+		System.out.println(aa.concat("Devaraj"));
+	}
+
+}
